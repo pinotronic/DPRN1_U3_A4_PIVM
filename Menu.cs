69da00c2a7f1f0603a187f176d815110b7bdfc2b
pinotronic <pinotronic@gmail.com>
2020-09-06 15:15:53 -0500

    Actualizacion del menu
    Actualizacion del menu

--- Menu.cs
+++ Menu.cs
@@ -1,22 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
+using System.Collections.Generic;
+
+
+
 
 namespace DPRN1_U3_A4_PIVM
 {
     public class Menu
     {
-        public void menu()
-        {
-            Console.WriteLine("            ***************************************");
-            Console.WriteLine("            *                                     *");
-            Console.WriteLine("            *          Actividad 4 Unidad 3       *");
-            Console.WriteLine("            *      Pino Martin Vargas Marquez     *");
-            Console.WriteLine("            *             ES172005165             *");
-            Console.WriteLine("            *                                     *");
-            Console.WriteLine("            ***************************************");
-            Console.WriteLine("\n             Programa Caja de Tienda Deportiva");
-        }
-    }
+        public void Menu1()
 
+        {
+            Console.WriteLine("  ████████████████████████████████████████████████████████████████████████████████████████████████████████████████████    ");
+            Console.WriteLine("  ███                                                                                                              ███    ");
+            Console.WriteLine("  ███                                           Caja de Tienda Deportiva                                           ███    ");
+            Console.WriteLine("  ███                                                                                                              ███    ");
+            Console.WriteLine("  ███                                              1.-REGITRAR ARTICULOS                                           ███    ");
+            Console.WriteLine("  ███                                              2.-MOSTRAR ARTICULOS                                            ███    ");
+            Console.WriteLine("  ███                                              3.-CAJA                                                         ███    ");
+            Console.WriteLine("  ███                                              4.-MOSTRAR VENTAS                                               ███    ");
+            Console.WriteLine("  ███                                              5.-SALIR                                                        ███    ");
+            Console.WriteLine("  ███                                                                                                              ███    ");
+            Console.WriteLine("  ███                                          Actividad 4  Unidad 3                                               ███    ");
+            Console.WriteLine("  ███                                        Pino Martin Vargas Marquez                                            ███    ");
+            Console.WriteLine("  ███                                               ES172005165                                                    ███    ");
+            Console.WriteLine("  ████████████████████████████████████████████████████████████████████████████████████████████████████████████████████    ");
+
+        }
+
+
+    }
 }
